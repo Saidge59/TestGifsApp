22821a89f5fe6487050d26c8885e19fc04bb7d08
Denis Shesternyak <shesternyakdenis@gmail.com>
2023-09-05 09:21:13 +0300

    Append 'RecyclerView' in MainActivity.java

--- app/src/main/java/com/example/testgifsapp/MainActivity.java
+++ app/src/main/java/com/example/testgifsapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.testgifsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rView = findViewById(R.id.recyclerView);
+        rView.setLayoutManager(new GridLayoutManager(this, 3));
+        rView.setHasFixedSize(true);
     }
 }
